131c6bde3404c60c3a2585f4eac636886e1b65c0
Mayramejiaa <mayri_isa96@hotmail.com>
2022-09-27 19:51:59 -0400

    second commit

--- src/class03/class03.java
+++ src/class03/class03.java
@@ -3,5 +3,6 @@ package class03;
 public class class03 {
     public static void main (String [] args){
     System.out.println("hey this is a git class");
+        System.out.println("hey this isa change in my project ");
 }
 }
